335877e0ad4d119cd9215d9aa42a69065659088d
Jon Wills <Jonax@users.noreply.github.com>
2022-12-01 20:44:07 +0000

    Day01 Refinement
    The functions for both parts can be merged into a generalised function...so let's do that.

--- Day01.py
+++ Day01.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 
 def IterateElves(calories):
 	current = []
@@ -16,21 +15,15 @@ def IterateElves(calories):
 	if any(current):
 		yield current
 
-def Part1(inputFile):
-	with open(inputFile) as inFile:
-		data = [l.strip() for l in inFile.readlines()]
-
-	return max(sum(e) for e in IterateElves(data))
-
-def Part2(inputFile):
+def Solution(inputFile, n = 1):
 	with open(inputFile) as inFile:
 		data = [l.strip() for l in inFile.readlines()]
 	
-	return sum(sorted([sum(e) for e in IterateElves(data)], reverse = True)[:3])
+	return sum(sorted([sum(e) for e in IterateElves(data)], reverse = True)[:n])
 
 def test():
-	assert Part1("Input/Day01_Example.txt") == 24000
-	print(f"Part 1: {Part1('Input/Day01.txt')}")
+	assert Solution("Input/Day01_Example.txt") == 24000
+	print(f"Part 1: {Solution('Input/Day01.txt')}")
 
-	assert Part2("Input/Day01_Example.txt") == 45000
-	print(f"Part 2: {Part2('Input/Day01.txt')}")
+	assert Solution("Input/Day01_Example.txt", 3) == 45000
+	print(f"Part 2: {Solution('Input/Day01.txt', 3)}")
